773e2a5f4e487b42fff0dbddeb1cd8c37b44f27d
thekightkeeper1 <ty.singh828@gmail.com>
2025-11-24 13:20:22 -0500

    Add testing file

---
+++ yahtzee/testing.c
@@ -0,0 +1,29 @@
+#include  "game.h"
+#include <stdio.h>
+
+// Function for testing
+void test_dice() {
+
+}
+
+
+// Runs some basic testing stuff. Pass args to it to test particular things?
+int main(int argc, char *argv[]) {
+    const char* arg = argv[1];
+
+    if (arg == NULL) {
+        printf("no args provided \n");
+        exit(1);
+    }
+
+
+
+    int caseNum = atoi(arg);
+    switch (caseNum) {
+        case 1:
+            break;
+        default:
+            break;
+    }
+
+}

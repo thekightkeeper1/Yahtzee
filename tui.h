da5a598a4c30d8d6c6210a39d36f7f6c47891621
thekightkeeper1 <ty.singh828@gmail.com>
2025-11-24 23:16:06 -0500

    Refactor constants to global namespace

--- tui.h
+++ tui.h
@@ -5,10 +5,9 @@
 #define _XOPEN_SOURCE_EXTENDED /* Required for ncurses to actually use the unicode funcs MUST GO FIRST*/
 
 #include "wchar.h"
-#include <ncursesw/ncurses.h>
+#include "./yahtzee/game.h"
 
 typedef wchar_t* wstr_t;
-
 typedef struct {
     
     // Also, to know location of each cell, we need to know the sum of widths before it, hence the cummulative widths.
@@ -29,15 +28,34 @@ typedef enum {
     LABEL_CELL
 } color_pair_t;
 
+// Labels used for the score table. Fine to change this the table is all dynamic.
+const wstr_t YAHTZEE_HANDS[NUM_CATEGORIES] = {
+    L"Ones",
+    L"Twos",
+    L"Threes",
+    L"Fours",
+    L"Fives",
+    L"Sixes",
+    L"Three of a Kind",
+    L"Four of a Kind",
+    L"Small Straight",
+    L"Large Straight",
+    L"Full House",
+    L"Yahtzee",
+    L"Upper total",
+    L"Lower total",
+    L"Total",
+};
 
-void get_cell_pos(const int r, const int c, int* x, int* y, const Table t);
+
+Table make_autosized_table(wstr_t **columns, int numCols, int columnHeight);
+void get_cell_pos(int r, int c, int* x, int* y, Table t);
 void draw_table(Table t);
 int maxlen(wchar_t **arr, int n);
-void repeat(wchar_t* dest, const wchar_t c, const int n);
+void repeat(wchar_t* dest, wchar_t c, int n);
 void malloc_failed();
-Table make_autosized_table(wstr_t **columns, int numCols, int columnHeight);
 void free_autosized_table(Table t);
-void draw_table_data(const Table t);
+void draw_table_data(Table t);
 void draw_cell_data(Table t, int c, int r, color_pair_t color);
 void test_menu(wchar_t** choices_w, int n_choices);
 

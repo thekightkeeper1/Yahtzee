5f2526f8305b050242268c46c84661283542dcf3
thekightkeeper1 <ty.singh828@gmail.com>
2025-11-24 23:21:38 -0500

    remove bad menu test

--- tui.c
+++ tui.c
@@ -3,56 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ncursesw/menu.h>
-
-void test_menu(wchar_t** choices_w, int n_choices) {
-
-    char **choices = malloc(sizeof(char*) * n_choices);
-    for (int i = 0; i < n_choices; i++) {
-        choices[i] = malloc(4 * wcslen(choices_w[i]));
-        wcstombs(choices[i], choices_w[i], wcslen(choices_w[i]) * 4);
-
-    }
-
-    ITEM **my_items;
-    int c;
-    MENU *my_menu;
-    int i;
-    ITEM *cur_item;
-
-    keypad(stdscr, TRUE);
-    my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
-
-    for(i = 0; i < n_choices; ++i)
-        my_items[i] = new_item(choices[i], choices[i]);
-    my_items[n_choices] = (ITEM *)NULL;
-
-    move(3, 60);
-    my_menu = new_menu((ITEM **)my_items);
-    // mvprintw(LINES - 2, 0, "F1 to Exit");
-    post_menu(my_menu);
-    refresh();
-
-    while((c = getch()) != KEY_F(1))
-    {   switch(c)
-    {	case KEY_DOWN:
-            menu_driver(my_menu, REQ_DOWN_ITEM);
-            break;
-        case KEY_UP:
-            menu_driver(my_menu, REQ_UP_ITEM);
-            break;
-    }
-    }
-
-    free_item(my_items[0]);
-    free_item(my_items[1]);
-    free_menu(my_menu);
-    for ( i = 0; i < n_choices; i++) {
-        free(choices[i]);
-    }
-    free(choices);
-    endwin();
-}
-
+#include <ncursesw/ncurses.h>
 
 void malloc_failed() {
     endwin();
